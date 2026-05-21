b7db56ca159cce2f2b7675438195d658bcae56e8
Saint-Thembani <st10528243@rcconnect.edu.za>
2026-05-21 06:51:09 +0200

    Part 2 Third commit

--- src/main/java/com/mycompany/chatapp/Messages.java
+++ src/main/java/com/mycompany/chatapp/Messages.java
@@ -12,6 +12,10 @@ public class Messages {
     String message;
     String firstWord = "";
     String secondWord = "";
+     int messageAmount;
+     String messageDetails;
+         String continueChoice;
+
 
     String[] sentMessages = new String[100];
     int messageCount = 0;
@@ -82,70 +86,82 @@ public class Messages {
         return true;
     }
 
-    void sendMessage() {
+void sendMessage() {
+    do {
 
-        System.out.println("How many messages do you want to send?");
-        int messageAmount = input.nextInt();
+        getRecipientCell();
+
+        while (!checkRecipientCell()) {
+            System.out.println("Invalid recipient number.");
+            getRecipientCell();
+        }
+
+        System.out.println("Please enter your message:");
+        message = input.nextLine();
+
+        while (message.length() > 250) {
+
+            int extra = message.length() - 250;
+
+            System.out.println("Message exceeds 250 characters by "
+                    + extra + " characters.");
+
+            System.out.println("Please re-enter message:");
+            message = input.nextLine();
+        }
+
+        System.out.println("Message ready to send.");
+
+        System.out.println("1. Send message");
+        System.out.println("2. Disregard message");
+        System.out.println("3. Store message");
+
+        int option = input.nextInt();
         input.nextLine();
 
-        int i = 1;
+        switch (option) {
+
+            case 1 -> {
+                sentMessages[messageCount] = message;
+                messageCount++;
+
+                System.out.println("Message sent successfully.");
+                System.out.println("Message Hash: " + createHashmap());
+                System.out.println(displayMessage());
+            }
+
+            case 2 -> {
+                System.out.println("Message deleted.");
+            }
+
+            case 3 -> {
+                sentMessages[messageCount] = message;
+                messageCount++;
+
+                System.out.println("Message stored successfully.");
+               
+            }
+
+            default -> {
+                System.out.println("Invalid option.");
+            }
+        }
 
         do {
-            getRecipientCell();
+            System.out.println("Do you want to enter another message? (yes/no)");
+            continueChoice = input.nextLine();
 
-            while (!checkRecipientCell()) {
-                System.out.println("Invalid recipient number.");
-                getRecipientCell();
-            }
+        }while (!continueChoice.equalsIgnoreCase("yes")
+                    && !continueChoice.equalsIgnoreCase("no"));
 
-            System.out.println("Please enter message " + i + ": ");
-            message = input.nextLine();
 
-            while (message.length() > 250) {
-                int extra = message.length() - 250;
 
-                System.out.println("Message exceeds 250 characters by "
-                        + extra + " characters.");
+        } while (continueChoice.equalsIgnoreCase("yes"));
 
-                System.out.println("Please re-enter message:");
-                message = input.nextLine();
-            }
+        System.out.println("Total messages sent: " + messageCount);
+}
 
-            System.out.println("Message ready to send.");
-
-            System.out.println("1. Send message");
-            System.out.println("2. Disregard message");
-            System.out.println("3. Store message");
-
-            int option = input.nextInt();
-            input.nextLine();
-
-            switch (option) {
-
-                case 1 -> {
-                    sentMessages[messageCount] = message;
-                    messageCount++;
-
-                    System.out.println("Message sent successfully.");
-                    System.out.println("Message Hash: " + createHashmap());
-                }
-                case 2 -> {
-                    System.out.println("Message deleted.");
-                }
-                case 3 -> {
-                    sentMessages[messageCount] = message;
-                    messageCount++;
-                    System.out.println("Message stored successfully.");
-                }
-                default -> {
-                    System.out.println("Invalid option.");
-                }
-            }
-            i++;
-        } while (i <= messageAmount);
-    }
-
-    String createHashmap() {
+            String createHashmap() {
 
         long result = messageID;
         while (result >= 100) {
@@ -156,7 +172,7 @@ public class Messages {
             firstWord = words[0];
             secondWord = words[1];
         }
-        return result + ":" + firstWord + secondWord;
+        return result + ":" + messageAmount+ firstWord + secondWord;
     }
 
     void printMessages() {
@@ -169,5 +185,11 @@ public class Messages {
         for (int i = 0; i < messageCount; i++) {
             System.out.println((i + 1) + ". " + sentMessages[i]);
         }
+ 
     }
+        String displayMessage() {
+
+            return "Message ID: " + messageID+ "\nMessage Hash: " + createHashmap()+
+                    "\nRecipient: " + recipientCell+ "\nMessage: " + message;
 }
+    }
